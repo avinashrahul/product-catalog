c915aa0eb062e8c24b65f5f9a00eca7940a10ea1
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-08-21 15:56:06 -0600

    update puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,26 @@
 set :application, 'product-catalog'
 set :repo_url, 'git@github.com:avinashrahul/product-catalog.git' # Edit this to match your repository
 set :branch, :master
-#set :deploy_to, '/home/deploy/product-catalog'
 set :deploy_to, '/var/www/product-catalog'
-set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.2.2'
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
+set :rvm_ruby_version, 'ruby-2.2.2'
+set :rvm_type, :auto                     # Defaults to: :auto
+set :rvm_roles, [:app, :web]
+
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 
 namespace :deploy do
 
@@ -38,14 +35,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
+      puts '** restarting'
       before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
