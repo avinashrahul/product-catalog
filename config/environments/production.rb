c66d277d1d3e38c4388cec5705acf46e8abfc4ee
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-08-21 16:19:44 -0600

    Update Production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.precompile += %w[active_admin.css active_admin.js]
   config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
